d9ce37446924790826a51aedaec50a1ab68462cd
music_apprentice66 <jyhuang@umass.edu>
2024-03-25 18:43:04 -0400

    commented socket.io

--- server/modules/auth.js
+++ server/modules/auth.js
@@ -1,13 +1,20 @@
 function verifyStudentData(socket) {
+    //socket.on listens for the event studentInfo which should send us both studentName and studentPassword
     socket.on('studentInfo', (studentName, studentPassword) => {
+
+        //call the function to search the username and password up in the database
         let studentVerified = studentMatchDB(studentName, studentPassword);
         console.log(studentVerified);
+
+        //socket.emit sends the event studentVerification, which the front-end can listen for using socket.on
+        //we send student verified, a boolean of whether or not the student name and password was verified
         socket.emit("studentVerification", studentVerified);
         return studentVerified;
     });
 }
 
 function studentMatchDB(studentName, studentPassword) {
+    //dummy holder function
     console.log("studentName", studentName);
     console.log("studentPassword", studentPassword);
     let studentsArr = ["Maya", "Jason", "Bach"];
