204d01b53112692740723a2296926633f28452c7
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-03-26 17:26:31 -0400

    Update server.test.js

--- server/server.test.js
+++ server/server.test.js
@@ -3,12 +3,46 @@ const app = require('./index.js');
 
 jest.mock('mongoose')
 
-describe('API Endpoints', () => {
-    it('GET /api - success', async () => {
-      const response = await request(app).get('/api');
-      expect(response.statusCode).toEqual(200);
-      expect(response.body).toEqual({
-        message: "Node.js server says hi to React Frontend!"
-      });
-    });
+describe('the function should add new teacher',() =>{
+  it('Test create teacher', async() => {
+    mongo = require('./insertDummyData.js');
+    const fisrName = "huy";
+    const lastName = "Gu";
+    const email = "hugy@gmaki.sut";
+  const password = "uhkjhku";
+  
+  await mongo.createTeacher(fisrName, lastName, email, password);
+  let re = await mongo.verifyTeacher(email, password);
+  expect(re).toEqual(true);
   });
+});
+ describe('the function should verify teacher',() =>{
+   it('Test existing teacher with wrong password', async() => {
+     mongo = require('./insertDummyData.js');
+     const email = "James.Moore@gmail.com";
+   const password = "wemKmh";
+   let re = await mongo.verifyTeacher(email, password);
+   expect(re).toEqual(false);
+   });
+   it('Test existing teacher with wrong email', async() => {
+    mongo = require('./insertDummyData.js');
+    const email = "JamesMoore@gmail.com";
+  const password = "wemKmhTh"
+  let re = await mongo.verifyTeacher(email, password);
+  expect(re).toEqual(false);
+  });
+  it('Test existing teacher with wrong email and password', async() => {
+    mongo = require('./insertDummyData.js');
+    const email = "JamesMoore@gmail.com";
+  const password = "wemKmh"
+  let re = await mongo.verifyTeacher(email, password);
+  expect(re).toEqual(false);
+  });
+  it('Test existing teacher', async() => {
+    mongo = require('./insertDummyData.js');
+    const email = "James.Moore@gmail.com";
+  const password = "wemKmhTh";
+  let re = await mongo.verifyTeacher(email, password);
+  expect(re).toEqual(true);
+  });
+ });
