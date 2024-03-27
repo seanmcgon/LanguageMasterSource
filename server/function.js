5780d0ea29401b0d637f7b41ae9dce4c420c5980
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-03-26 23:01:47 -0400

    Create function.js

---
+++ server/function.js
@@ -0,0 +1,74 @@
+
+const { MongoClient } = require('mongodb');
+
+const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(connectionString);
+
+function checkValidityOfEmail(emailAddress){
+  const regex = /^[^ ]+@[^ ]+\.[a-z]{2,}$/;
+  if(emailAddress.match(regex)){
+    return true;
+  }
+  return false;
+}
+  function checkValidityOfPassword(password){
+    const regex = /^[^ ]{2,15}$/;
+    if(password.match(regex)){
+      return true;
+    }
+    return false;
+  }
+
+async function verifyTeacher(teacherEmail, password){
+  try{
+    await client.connect();
+    db = client.db("UserData");
+  col = await db.collection("teachers");
+  let result = await col.find({$and:[{email: teacherEmail}, {password: password}]}).toArray();
+  return result.length == 1 ? true: false;
+  }
+  finally{
+  await client.close();
+  }
+}
+
+
+async function createTeacher(firstName,lastName,teacherEmail, password){
+  let re = verifyTeacher(teacherEmail.trim(), password.trim());
+  let boo = false;
+  boo = await re.then(e => e);
+  try{
+  if(!boo){
+    await client.connect();
+    db = client.db("UserData");
+    col = await db.collection("teachers");
+    const booE = checkValidityOfEmail(teacherEmail);
+  const booP = checkValidityOfPassword(password);
+    let courses = [];
+    if(booE && booP){
+    let result = {name: firstName.trim() + " " + lastName.trim(), email: teacherEmail.trim(),password: password.trim(),courseList: courses};
+    await col.insertOne(result);
+    console.log("Successfully create new teacher");
+    }
+    else if(!booE && booP){
+      throw("Email is invalid");
+    }
+    else if(booE && !booP){
+      throw("Pass is invalid");
+    }
+    else{
+      throw("Both are invalid");
+    }
+  }
+  else{
+    throw ("Teacher already exist!");
+  }  
+}
+  catch (err){
+    console.log(err);
+  }
+  finally{
+  await client.close();
+  }
+  }
+  module.exports = {createTeacher, verifyTeacher};
