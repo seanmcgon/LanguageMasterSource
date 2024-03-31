6136f0982c7a555a192eaf404099054f4a831c4a
Sean McGonigle <smcgonigle@umass.edu>
2024-03-31 15:27:02 -0400

    Updated comments in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
       {/* It takes the className string submitted by the user and should send that to the server(?) */}
       {/* Right now I have it set up to handle a return of true (success) or false (failure) */}
       {/* On success, exit the popup and show updated classes page/menu */}
-      {/* On failure (invalid class name), display a message prompting user to try a different name */}
+      {/* On failure (invalid class name / already exists), display a message prompting user to try a different name */}
       <CreateClassPopup
         show={modalShow}
         onHide={() => setModalShow(false)}
