d90a6486e42f331f7baa1303314cebfcf7d2156c
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-04-03 16:10:10 -0400

    Update server.test.js

--- server/server.test.js
+++ server/server.test.js
@@ -1,50 +1,90 @@
+const { MongoClient } = require('mongodb');
 const request = require('supertest');
 const app = require('./index.js'); 
-
+const { createTeacher, verifyTeacher, createClass } = require('./databaseUsers.js');
 jest.mock('mongoose')
 
 describe('the function should add new teacher',() =>{
   it('Test create teacher', async() => {
-    mongo = require('./function.js');
-    const fisrName = "huy";
+    const firstName = "huy";
     const lastName = "Gu";
-    const email = "hugy@gmaki.sut";
-  const password = "uhkjhku";
+    const email = "hugfdsfy@gmail.sut";
+    const password = "uhkfdsfj";
   
-  await mongo.createTeacher(fisrName, lastName, email, password);
-  let re = await mongo.verifyTeacher(email, password);
-  expect(re).toEqual(true);
-  });
+    await createTeacher(firstName, lastName, email, password);
+    let re = await verifyTeacher(email, password);
+    expect(re).toEqual(true);
+  }, 100000); 
+  it('Test existing teacher by his email and different password', async() => {
+    const firstName = "huy";
+    const lastName = "Gu";
+    const email = "hugfdsfy@gmail.sut";
+    const password = "uhkfkdlafda";
+  
+    await createTeacher(firstName, lastName, email, password);
+    let re = await verifyTeacher(email, password);
+    expect(re).toEqual(false);
+  }, 100000); 
 });
  describe('the function should verify teacher',() =>{
-   it('Test existing teacher with wrong password', async() => {
-     mongo = require('./function.js');
-     const email = "James.Moore@gmail.com";
-   const password = "wemKmh";
+   it('Test existing teacher ', async() => {
+     mongo = require('./databaseUsers.js');
+     const email = "jyhuang@umass.edu";
+   const password = "Test";
    let re = await mongo.verifyTeacher(email, password);
-   expect(re).toEqual(false);
+   expect(re).toEqual(true);
    });
    it('Test existing teacher with wrong email', async() => {
-    mongo = require('./function.js');
+    mongo = require('./databaseUsers.js');
     const email = "JamesMoore@gmail.com";
   const password = "wemKmhTh"
   let re = await mongo.verifyTeacher(email, password);
   expect(re).toEqual(false);
   });
   it('Test existing teacher with wrong email and password', async() => {
-    mongo = require('./function.js');
+    mongo = require('./databaseUsers.js');
     const email = "JamesMoore@gmail.com";
   const password = "wemKmh"
   let re = await mongo.verifyTeacher(email, password);
   expect(re).toEqual(false);
   });
-  it('Test existing teacher', async() => {
-    mongo = require('./function.js');
-    const email = "James.Moore@gmail.com";
-  const password = "wemKmhTh";
-  let re = await mongo.verifyTeacher(email, password);
-  expect(re).toEqual(true);
-  });
  });
 
+ describe('this suit will test the function createClass',() =>{
+  it('Add a class to a given teacher, and vice versa. In this case, the class already existed', async() => {
+    const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
+    const client = new MongoClient(connectionString);
+    const email = "jyhuang@umass.edu";
+    const className ="Vietnamese219_VJTCBB"; 
+  await createClass(className, email);
+  await client.connect();
+    db = client.db("UserData");
+    db1 = client.db(className);
+    col = await db.collection("teachers");
+    col1 = await db1.collection("teachers");
+  const testTeacher = (await col.find({email: email}).toArray())[0].courseList.indexOf(className);
+  const testClassDataBase = await col1.find({email:email}).toArray();
+  expect(testTeacher).toBeGreaterThan(-1);
+  expect(testClassDataBase.length).toBeGreaterThan(0);
+  });
+
+  // test case 2
+  
+  it('Add a class to a given teacher, and vice versa. In this case, the class does not exist', async() => {
+    
+    const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
+    const client = new MongoClient(connectionString);
+    await client.connect();
+    const email = "jyhuang@umass.edu";
+    const className ="LeageOfLegend_101";
+  await createClass(className, email);
+   db = client.db("UserData");
+    col = await db.collection("teachers");
+    const testTeacher = (await col.find({email: email}).toArray())[0].courseList.indexOf(className);
+  
+  //const testClassDataBase = await col1.find({email:email}).toArray();
+  //await client.close();
+  expect(testTeacher).toBeGreaterThan(-1);
+  });
+});
 
