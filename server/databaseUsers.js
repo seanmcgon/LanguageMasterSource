f406865049506244cfcc9f8306fd416f49d18a22
music_apprentice66 <jyhuang@umass.edu>
2024-04-05 17:01:05 -0400

    Shuto and Quoc backend functions updated, minus transcription

--- server/databaseUsers.js
+++ server/databaseUsers.js
@@ -1,92 +1,162 @@
-
+const { assert } = require('console');
 const { MongoClient } = require('mongodb');
 const { TextEncoder } = require('util');
 
 const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
 const client = new MongoClient(connectionString);
 
-function checkValidityOfEmail(emailAddress){
-  const regex = /^[^ ]+@[^ ]+\.[a-z]{2,}$/;
-  if(emailAddress.match(regex)){
-    return true;
-  }
-  return false;
-}
-  function checkValidityOfPassword(password){
-    const regex = /^[^ ]{2,15}$/;
-    if(password.match(regex)){
-      return true;
+function checkValidityOfEmail(emailAddress) {
+    const regex = /^[^ ]+@[^ ]+\.[a-z]{2,}$/;
+    if (emailAddress.match(regex)) {
+        return true;
     }
     return false;
-  }
-
-async function verifyTeacher(teacherEmail, password){
-  try{
-    await client.connect();
-    db = client.db("UserData");
-    col = await db.collection("teachers");
-    let result = await col.find({$and:[{email: teacherEmail}, {password: password}]}).toArray();
-    return result.length == 1 ? true: false;
-  }
-  finally{
-  await client.close();
-  }
-}
-async function verifyStudent(studentEmail, password){
-  try{
-    await client.connect();
-    db = client.db("UserData");
-  col = await db.collection("students");
-  let result = await col.find({$and:[{email: studentEmail}, {password: password}]}).toArray();
-  return result.length == 1 ? true: false;
-  }
-  finally{
-  await client.close();
-  }
 }
 
-
-async function createTeacher(firstName,lastName,teacherEmail, password){
-  let createdTeacher = false;
-
-  let re = verifyTeacher(teacherEmail.trim(), password.trim());
-  let boo = false;
-  boo = await re.then(e => e);
-  try{
-  if(!boo){
-    await client.connect();
-    db = client.db("UserData");
-    col = await db.collection("teachers");
-    const booE = checkValidityOfEmail(teacherEmail);
-    const booP = checkValidityOfPassword(password);
-    let courses = [];
-
-    if(booE && booP){
-      let result = {name: firstName.trim() + " " + lastName.trim(), email: teacherEmail.trim(),password: password.trim(),courseList: courses};
-      await col.insertOne(result);
-      createdTeacher = True;
-      console.log("Successfully create new teacher",teacherEmail, password);
+function checkValidityOfPassword(password) {
+    const regex = /^[^ ]{2,15}$/;
+    if (password.match(regex)) {
+        return true;
     }
-    else if(!booE && booP){
-      throw("Email is invalid");
-    }
-    else if(booE && !booP){
-      throw("Pass is invalid");
-    }
-    else{
-      throw("Both are invalid");
-    }
-  }
-  else{
-    throw ("Teacher already exist!");
-  }  
+    return false;
 }
-  catch (err){
-    console.log(err);
+
+async function verifyTeacher(teacherEmail, password) {
+    try {
+        await client.connect();
+        const db = client.db("UserData");
+        const col = await db.collection("teachers");
+        const result = await col.find({ $and: [{ email: teacherEmail }, { password: password }] }).toArray();
+        return result.length === 1;
+    } finally {
+        await client.close();
+    }
+}
+
+async function createTeacher(firstName, lastName, teacherEmail, password) {
+    let createdTeacher = false;
+
+    const re = verifyTeacher(teacherEmail.trim(), password.trim());
+    let boo = await re;
+    if (!boo) {
+        await client.connect();
+        const db = client.db("UserData");
+        const col = await db.collection("teachers");
+        const doubleE = await col.find({ email: teacherEmail }).toArray();
+        const booE = checkValidityOfEmail(teacherEmail);
+        const booP = checkValidityOfPassword(password);
+        let courses = [];
+
+        if (booE && booP && doubleE.length !== 1) {
+            const result = { name: `${firstName.trim()} ${lastName.trim()}`, email: teacherEmail.trim(), password: password.trim(), courseList: courses };
+            await col.insertOne(result);
+            createdTeacher = true;
+            console.log("Successfully created new teacher");
+        }
+
+        await client.close();
+    }
+    return createdTeacher;
+}
+
+function checkValid(className) {
+    const regex = /^[^ ]+\_[^ ]{1,6}$/;
+    if (className.match(regex)) {
+        return true;
+    }
+    return false;
+}
+
+async function updateClassForGivenTeacher(col, teacherEmail, className) {
+    const courseL = await col.find({ email: teacherEmail }).toArray();
+    let originalCourse = courseL[0].courseList;
+    if (!originalCourse.includes(className)) {
+        originalCourse.push(className);
+    }
+    await col.updateOne({ email: teacherEmail }, { $set: { courseList: originalCourse } });
+}
+
+async function createClass(className, teacherEmail) {
+    let classCreated = false;
+
+    await client.connect();
+    const db = client.db("UserData");
+    const col = await db.collection("teachers");
+    const checkTheValidOfClassName = checkValid(className);
+
+    const allCoursesPipeline = [
+        { $unwind: "$courseList" },
+        { $group: { _id: null, allCourses: { "$push": "$courseList" } } },
+        { '$addFields': { 'courseList': { '$setUnion': ['$fcourseList', []] } } }
+    ];
+
+    let courses = await col.aggregate(allCoursesPipeline);
+    let c = await courses.next();
+
+    if (checkTheValidOfClassName) {
+        if ((await col.find({ email: teacherEmail }).toArray()).length === 1) {
+            await updateClassForGivenTeacher(col, teacherEmail, className);
+            let getTeacherInfo = await col.find({ email: teacherEmail }).toArray();
+            if (c.allCourses.includes(className)) {
+                const db1 = client.db(className);
+                const col1 = await db1.collection("teachers");
+                const teacherInThatClass = await col.find({ email: teacherEmail }).toArray();
+                if (teacherInThatClass.length !== 1) {
+                    await col1.insertOne(getTeacherInfo[0]);
+                } else {
+                    await col1.deleteOne({ email: teacherEmail });
+                    await col1.insertOne(getTeacherInfo[0]);
+                }
+                classCreated = true;
+            } else {
+                MongoClient.connect(connectionString).then(async (client) => {
+                    const db = client.db(className);
+                    db.createCollection("assignments");
+                    db.createCollection("metrics");
+                    db.createCollection("students");
+                    db.createCollection("teachers");
+                    col = db.collection("teachers");
+                    await col.insertOne(getTeacherInfo[0]);
+                    await client.close();
+                    classCreated = true;
+                });
+            }
+        }
+    }
+
+    return classCreated;
+}
+
+async function getClassesTeacher(teacherEmail) {
+  try {
+    await client.connect();
+    const db = client.db("UserData");
+    const col = db.collection("teachers");
+    const result = await col.findOne({email: teacherEmail.trim() });
+    if (result) {
+      return result.courseList || [];
+    } else {
+      return [];
+    }
+  } finally {
+    await client.close();
   }
-  finally{
-  await client.close();
-  return createdTeacher;
+}
+
+async function getClassesStudent(studentEmail) {
+  try {
+    await client.connect();
+    const db = client.db("UserData");
+    const col = db.collection("students");
+    const result = await col.findOne({email: studentEmail.trim() });
+    if (result) {
+      return result.courseList || [];
+    } else {
+      return [];
+    }
+  } finally {
+    await client.close();
   }
-  }
-  module.exports = {createTeacher, verifyTeacher};
+}
+
+module.exports = { createTeacher, verifyTeacher, createClass, getClassesTeacher, getClassesStudent}
--- server/server.test.js
+++ server/server.test.js
@@ -1,49 +1,106 @@
+const { MongoClient } = require('mongodb');
 const request = require('supertest');
-const app = require('./index.js'); 
-const { createTeacher, verifyTeacher } = require('./function.js');
-jest.mock('mongoose')
+const app = require('./index.js');
+const { createTeacher, verifyTeacher, createClass,getClassesStudent, getClassesTeacher} = require('./databaseUsers.js');
+jest.mock('mongoose');
 
-describe('the function should add new teacher',() =>{
-  it('Test create teacher', async() => {
-    const firstName = "huy";
-    const lastName = "Gu";
-    const email = "hugy@gmail.sut";
-    const password = "uhkjhku";
-  
-    await createTeacher(firstName, lastName, email, password);
-    let re = await verifyTeacher(email, password);
-    expect(re).toEqual(true);
-  }, 100000); 
+describe('the function should add new teacher', () => {
+    it('Test existing teacher by his email and different password', async () => {
+        const firstName = "huy";
+        const lastName = "Gu";
+        const email = "huyGu@gmail.com";
+        const password = "password";
+    
+        await createTeacher(firstName, lastName, email, password);
+        let result = await verifyTeacher(email, 'notThePassword');
+        expect(result).toEqual(false);
+    }, 100000);
 });
- describe('the function should verify teacher',() =>{
-   it('Test existing teacher with wrong password', async() => {
-     mongo = require('./function.js');
-     const email = "James.Moore@gmail.com";
-   const password = "wemKmh";
-   let re = await mongo.verifyTeacher(email, password);
-   expect(re).toEqual(false);
-   });
-   it('Test existing teacher with wrong email', async() => {
-    mongo = require('./function.js');
-    const email = "JamesMoore@gmail.com";
-  const password = "wemKmhTh"
-  let re = await mongo.verifyTeacher(email, password);
-  expect(re).toEqual(false);
-  });
-  it('Test existing teacher with wrong email and password', async() => {
-    mongo = require('./function.js');
-    const email = "JamesMoore@gmail.com";
-  const password = "wemKmh"
-  let re = await mongo.verifyTeacher(email, password);
-  expect(re).toEqual(false);
-  });
-  it('Test existing teacher', async() => {
-    mongo = require('./function.js');
-    const email = "James.Moore@gmail.com";
-  const password = "wemKmhTh";
-  let re = await mongo.verifyTeacher(email, password);
-  expect(re).toEqual(true);
-  });
- });
 
+describe('the function should verify teacher', () => {
+    let mongo;
+    beforeAll(() => {
+        mongo = require('./databaseUsers.js');
+    });
 
+    it('Test existing teacher ', async () => {
+        const email = "jyhuang@umass.edu";
+        const password = "Test";
+        let result = await mongo.verifyTeacher(email, password);
+        expect(result).toEqual(true);
+    });
+
+    it('Test existing teacher with wrong email', async () => {
+        const email = "JamesMoore@gmail.com";
+        const password = "wemKmhTh";
+        let result = await mongo.verifyTeacher(email, password);
+        expect(result).toEqual(false);
+    });
+
+    it('Test existing teacher with wrong email and password', async () => {
+        const email = "JamesMoore@gmail.com";
+        const password = "wemKmh";
+        let result = await mongo.verifyTeacher(email, password);
+        expect(result).toEqual(false);
+    });
+});
+
+describe('this suite will test the function createClass', () => {
+    const connectionString = "mongodb+srv://mkandeshwara:0CgF5I8hwXaf88dy@cluster0.tefxjrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
+    let client, db, col;
+
+    beforeAll(async () => {
+        client = new MongoClient(connectionString);
+        await client.connect();
+        db = client.db("UserData");
+        col = await db.collection("teachers");
+    });
+
+    afterAll(async () => {
+        await client.close();
+    });
+
+    it('Add a class to a given teacher, and vice versa. In this case, the class already existed', async () => {
+        const email = "jyhuang@umass.edu";
+        const className = "Vietnamese219_VJTCBB";
+        await createClass(className, email);
+        const db1 = client.db(className);
+        const col1 = await db1.collection("teachers");
+        const testTeacher = (await col.find({ email: email }).toArray())[0].courseList.indexOf(className);
+        const testClassDataBase = await col1.find({ email: email }).toArray();
+        expect(testTeacher).toBeGreaterThan(-1);
+        expect(testClassDataBase.length).toBeGreaterThan(0);
+    });
+
+    it('Add a class to a given teacher, and vice versa. In this case, the class does not exist', async () => {
+        const email = "jyhuang@umass.edu";
+        const className = "LeageOfLegend_101";
+        await createClass(className, email);
+        const testTeacher = (await col.find({ email: email }).toArray())[0].courseList.indexOf(className);
+        expect(testTeacher).toBeGreaterThan(-1);
+    });
+});
+
+describe('getClassesTeacher', () => {
+  it('should return an array of classes for a teacher', async () => {
+    const classes = await getClassesTeacher('johndoe@gmail.com');
+    expect(Array.isArray(classes)).toBe(true);
+  });
+
+  it('should return an empty array if teacher does not exist', async () => {
+    const classes = await getClassesTeacher('nonexistentteacher');
+    expect(classes).toEqual([]);
+  });
+});
+
+describe('getClassesStudent', () => {
+  it('should return an array of classes for a student', async () => {
+    const classes = await getClassesStudent('Stephen.Calderon@gmail.com');
+    expect(Array.isArray(classes)).toBe(true);
+  });
+
+  it('should return an empty array if student does not exist', async () => {
+    const classes = await getClassesStudent('nonexistentstudent');
+    expect(classes).toEqual([]);
+  });
+});
