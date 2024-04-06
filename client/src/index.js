011f37803b8d5da93ab8d75cdd88633d0d01c52b
Sean McGonigle <smcgonigle@umass.edu>
2024-04-06 17:57:19 -0400

    Integrated routing into index.js to help with dev

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ClassAsgmts from './components/classAsgmts';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/class-asgmts" element={<ClassAsgmts />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
