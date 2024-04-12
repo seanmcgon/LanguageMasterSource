6a7e3523e175a8fcce78e6d738f1db38442a68ed
Sean McGonigle <smcgonigle@umass.edu>
2024-04-11 22:45:09 -0400

    idk

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = createServer(app);
 
 const port = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
