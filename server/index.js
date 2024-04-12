03e244a8b77d636248450f9944cf41fae517b525
Sean McGonigle <smcgonigle@umass.edu>
2024-04-11 23:10:47 -0400

    attempt to fix cors error

--- server/index.js
+++ server/index.js
@@ -6,7 +6,23 @@ var cors = require('cors')
 const app = express();
 //create a new Express application
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  request(
+    { url: 'https://languagemastersource-server.onrender.com' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
 
 const server = createServer(app);
 //app is passed to createServer which creates an HTTP server that uses the Express app to handle requests
