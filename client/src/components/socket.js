66230470bb72c47678e11792e82a501eaa05eff3
Sean McGonigle <smcgonigle@umass.edu>
2024-04-11 21:34:17 -0400

    change socket destination

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://languagemastersource-server.onrender.com:3001');
 
 export const connectSocket = () => {
   socket.on("connect", () => {
